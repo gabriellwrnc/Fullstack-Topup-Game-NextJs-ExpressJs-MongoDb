eca65f1758ace950f76cc593b6df0b66c21bff17
Gabriel Jamrewav <gabriel.jamrewav@gmail.com>
2023-03-24 12:15:16 +0800

    Landing page done (with animation)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "../styles/homepage.css";
+import "../styles/utilities.css";
+import "../styles/detail.css";
+import { useEffect } from "react";
+import AOS from "aos";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init();
+  });
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
